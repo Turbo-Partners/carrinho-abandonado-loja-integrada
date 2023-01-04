fb5f1a4a13af58fcb2c438e54f5f585ddc7733ef
Igor Nascimento Costa <69372874+IgorNCosta14@users.noreply.github.com>
2023-01-03 22:13:17 -0300

    Update server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -21,11 +21,7 @@ const io = new socketio.Server(httpServer, {
 });
 
 async function getPurchasesList () {
-  console.log("1")
-
   const dateFormatted = await formatDate();
-
-  console.log("2")
 
   await axios.get(`https://api.awsli.com.br/v1/pedido/search/?limit=20&since_atualizado=${dateFormatted}&chave_api=${process.env.CHAVE_API}&chave_aplicacao=${process.env.CHAVE_APLICACAO}`,{
     headers: {
@@ -46,6 +42,7 @@ async function getPurchasesList () {
         }
       })
       .then(function (response) {
+        console.log("4")
         purchaseData = response.data;
       })
       .catch(function (error) {
@@ -79,7 +76,7 @@ async function getPurchasesList () {
   });
 };
 
-setInterval(getPurchasesList, 60000);
+setInterval(getPurchasesList, 120000);
 
 app.post("/finalizacao/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
